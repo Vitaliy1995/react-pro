7e80ebc489947ced225ae7d163293691d211b3d2
Vitaliy Shevchuk <shevchukvitalik@bk.ru>
2023-02-17 18:43:18 +0300

    Visual for screenshot tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import {
-    renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar component', () => {
     test('sidebar in document', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
 
         const toggleButton = screen.getByTestId('sidebar-toggle');
 
