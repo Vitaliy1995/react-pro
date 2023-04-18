45f6fcbd0b0b7222013d26e8b04b1c56d6c35926
vshevchuk <shevchukvitalik@bk.ru>
2023-04-18 19:28:25 +0300

    Tests

---
+++ src/features/AuthByUserName/model/selectors/getLoginIsLoading/getLoginIsLoading.test.ts
@@ -0,0 +1,20 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginIsLoading } from './getLoginIsLoading';
+
+describe('getLoginError test', () => {
+    test('should return error', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                isLoading: true,
+            },
+        };
+
+        expect(getLoginIsLoading(state as StateSchema)).toEqual(true);
+    });
+
+    test('should work with empty', () => {
+        const state: DeepPartial<StateSchema> = {};
+
+        expect(getLoginIsLoading(state as StateSchema)).toEqual(false);
+    });
+});
