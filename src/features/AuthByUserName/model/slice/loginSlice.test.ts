bdaf0021b79b53b375068847d1925a3fc155feaf
vshevchuk <shevchukvitalik@bk.ru>
2023-04-18 19:28:55 +0300

    Fix tests

--- src/features/AuthByUserName/model/slice/loginSlice.test.ts
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -4,11 +4,11 @@ import { loginActions } from 'features/AuthByUserName/model/slice/loginSlice';
 describe('loginSlice test', () => {
     test('test set username', () => {
         const state: DeepPartial<LoginSchema> = { username: '123' };
-        expect(loginReducer(state as LoginSchema, loginActions.setUsername('123123'))).toBe({ username: '123123' });
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('123123'))).toEqual({ username: '123123' });
     });
 
     test('test set password', () => {
         const state: DeepPartial<LoginSchema> = { password: '123' };
-        expect(loginReducer(state as LoginSchema, loginActions.setPassword('123123'))).toBe({ password: '123123' });
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('123123'))).toEqual({ password: '123123' });
     });
 });
